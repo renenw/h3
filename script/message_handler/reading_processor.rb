058f38610330f7d7d50eed6d879b5c61a65b897f
renen <renen@121.co.za>
2012-09-13 19:56:59 +0000

    readings split off by themsleves

--- script/message_handler/reading_processor.rb
+++ script/message_handler/reading_processor.rb
@@ -8,6 +8,9 @@ require 'tzinfo'
 require './cacher'
 require 'pp'
 require './config'
+require './reading_handlers'
+
+include Reading_Handlers
 
 @mysql = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "30_camp_ground_road")
 @cache = Cacher.new('localhost:11211')
@@ -25,37 +28,6 @@ end
 
 =begin
 
-* todo
-
-bandwidth must be wrong. It's defined as a pulse meter, but the pulse handler converts everything to watt hours!
-
-* the data structure passed through the message chain
-
-Payload is gradually augmented.
-
-udp adds the time the packet was received
-udp_handler (handle_udp):
-  1. kills badly structured messages
-  2. records unkown (ie, not in config.rb) monitors in a dead-letter-list,
-  3. for valid messages, a data_store (location) is added, a local time is added, and the message is sent on to initialise_structured_message
-  4. if the message is a mrtg message, it is sent to handle_mrtg_pre_processing
-the various handle_xxxx handlers add a "converted_value" entry that is the result of assessing the value received in the payload
-
-the handle_xxx adds a "converted_value" entry that reflects the value to be used by subsequent operations.
-  
-* the queue flow
-
-udp
-  => udp_handler (handle_udp): an opportunity to rewrite messages, ditch them etc
-    => by default, initialise_structured_message (initialise_structured_message): messages should, by this point, have a basic "events" structure (source, number) with a received time that as closely as practically reflects the event time
-      => if :pulse, pulse (handle_pulse): converted_value set to watt_hours since last reading (by implication, only handles watt_hours)
-        => reading
-      => if :gauge, gauge (handle_gauge): converted_value set to "float value"
-        => reading
-      => if :counter, counter (handle_gauge)
-        => reading
-    => if :mrtg, handle_mrtg_pre_processing: which pushes out three new entries - bandwidth in, out and total - but does not pass itsel along for further processing (it commits suicide)
- 
   reading (handle_reading): saves the entry into the "readings" table and pushes the reading out into the fan out exchange
     => handle_summarisation: updates the summary data with this reading (eg, quarterly total, max, min etc)
     => handle_history: keeps a recent set of entries around in the cache (eg 200)
@@ -75,28 +47,28 @@ def de_queuer
       @exchange = channel.direct(RABBIT_EXCHANGE)
       @process_exchange = channel.fanout(RABBIT_PROCESS_EXCHANGE)
 
-      channel.queue('udp_handler', :auto_delete => false).subscribe do |message|
-        handle_udp(message)
+      channel.queue('reading', :auto_delete => false).subscribe do |message|
+        handle_reading(message)
       end
 
-      channel.queue('initialise_structured_message', :auto_delete => false).subscribe do |message|
-        initialise_structured_message(message)
+      channel.queue('summariser', :auto_delete => false).bind(@process_exchange).subscribe do |message|
+        handle_summarisation(message)
       end
 
-      channel.queue('handle_mrtg_pre_processing', :auto_delete => false).subscribe do |message|
-        handle_mrtg_pre_processing(message)
+      channel.queue('historifier', :auto_delete => false).bind(@process_exchange).subscribe do |message|
+        handle_history(message)
       end
 
-      channel.queue('counter', :auto_delete => false).subscribe do |message|
-        handle_gauge(message)
+      channel.queue('calculate_outlier_threshold', :auto_delete => false).bind(@process_exchange).subscribe do |message|
+        handle_calculate_outlier_threshold(message)
       end
 
-      channel.queue('gauge', :auto_delete => false).subscribe do |message|
-        handle_gauge(message)
+      channel.queue('cache_reading', :auto_delete => false).bind(@process_exchange).subscribe do |message|
+        handle_cache_reading(message)
       end
 
-      channel.queue('pulse', :auto_delete => false).subscribe do |message|
-        handle_pulse(message)
+      channel.queue('cache_sources', :auto_delete => false).bind(@process_exchange).subscribe do |message|
+        handle_cache_sources(message)
       end
 
     end
