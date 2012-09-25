2117f2012fa53273f96fd7f4097c2073f1926558
renen <renen@121.co.za>
2012-09-25 21:17:01 +0000

    improved naming and formating of monitors

--- script/message_handler/config.rb
+++ script/message_handler/config.rb
@@ -28,11 +28,12 @@ MONITORS = {
   'temperature_inside'  => { :monitor_type => :gauge, :expected_frequency => 300, :suffix => TEMPERATURE_SUFFIX  },
   'temperature_pool'    => { :monitor_type => :gauge, :range => { :min => 0, :max => 40}, :expected_frequency => 300, :suffix => TEMPERATURE_SUFFIX  },
   'bandwidth'           => { :monitor_type => :mrtg, :expected_frequency => 60  },
-  'bandwidth_in'        => { :monitor_type => :pulse, :range => { :min => 0, :max => Infinity}, :expected_frequency => 60 },
-  'bandwidth_out'       => { :monitor_type => :pulse, :range => { :min => 0, :max => Infinity}, :expected_frequency => 60 },
-  'bandwidth_total'     => { :monitor_type => :pulse, :range => { :min => 0, :max => Infinity}, :expected_frequency => 60 },
+  'bandwidth_in'        => { :monitor_type => :pulse, :range => { :min => 0, :max => Infinity}, :expected_frequency => 60, :suffix => ' bytes' },
+  'bandwidth_out'       => { :monitor_type => :pulse, :range => { :min => 0, :max => Infinity}, :expected_frequency => 60, :suffix => ' bytes' },
+  'bandwidth_total'     => { :monitor_type => :pulse, :range => { :min => 0, :max => Infinity}, :expected_frequency => 60, :suffix => ' bytes' },
   'outlier'             => { :monitor_type => :counter },
-  'alarm_alive'         => { :monitor_type => :keep_alive, :name => 'Alarm Monitor' },
+  'alarm_alive'         => { :monitor_type => :keep_alive, :name => 'Alarm and Pool Keep Alive', :expected_frequency => 60*60 },
+  'alarm_armed'         => { :monitor_type => :switch },
   'bandwidth_throughput'=> { :monitor_type => :gauge, :expected_frequency => 86400 }
 }
 
