09a8df6eed4a60a297308029b01f3eb834702969
renen <renen@121.co.za>
2012-10-10 19:41:01 +0000

    removed logging from twitter connection definition

--- script/message_handler/holler.rb
+++ script/message_handler/holler.rb
@@ -1,8 +1,7 @@
 module Holler
 
 	Twitter.configure do |config|
-		p 'Configuring twitter'
-	  config.consumer_key 			= TWITTER_CONSUMER_KEY
+		config.consumer_key 			= TWITTER_CONSUMER_KEY
 	  config.consumer_secret 		= TWITTER_CONSUMER_SECRET
 	  config.oauth_token 				= TWITTER_OAUTH_TOKEN
 	  config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
